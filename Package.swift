bc04dd59a128d2b4ab08933982e015040cc2e5ea
StatusQuo <bastihumann@googlemail.com>
2019-09-12 08:56:42 +0200

    define platform refactor

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "EasyLayout",
+    platforms: [.iOS(.v9), .tvOS(.v9), .watchOS(.v2)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
