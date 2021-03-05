3dffb9f7816c0678cc3ad8876ea46e30a8b49922
bastihumann@googlemail.com <bastihumann@googlemail.com>
2021-03-05 10:10:26 +0100

    :art: use resultBuilder

--- Sources/EasyLayout/EasyLayout.swift
+++ Sources/EasyLayout/EasyLayout.swift
@@ -1,91 +1,26 @@
 import Foundation
-import UIKit
+#if !os(macOS)
+    import UIKit
 
-extension UIView {
-    public func layout(@ListBuilder build: () -> Listable) {
-        if let list = build().asList() as? [NSLayoutConstraint] {
-            NSLayoutConstraint.activate(list)
+    extension NSLayoutConstraint {
+        static func activate(@ListBuilder <NSLayoutConstraint> build: () -> [NSLayoutConstraint]) {
+            NSLayoutConstraint.activate(build())
         }
     }
-}
 
-extension UIViewController {
-    public func layout(@ListBuilder build: () -> Listable) {
-        if let list = build().asList() as? [NSLayoutConstraint] {
-            NSLayoutConstraint.activate(list)
+    extension UIView {
+        @discardableResult
+        public func addSubview(@ListBuilder <UIView> build: () -> [UIView]) -> UIView {
+            build().forEach { addSubview($0) }
+            return self
         }
     }
-}
 
-extension UIView {
-    public func add(@ListBuilder build: () -> Listable) -> UIView {
-        if let list = build().asList() as? [UIView] {
-            list.forEach { view in
-                view.translatesAutoresizingMaskIntoConstraints = false
-                self.addSubview(view)
-            }
+    extension UIStackView {
+        public func addArrangedSubview(@ListBuilder <UIView> build: () -> [UIView]) -> UIStackView {
+            build().forEach { addArrangedSubview($0) }
+            return self
         }
-        return self
     }
-}
 
-extension UIStackView {
-    public func arrange(@ListBuilder build: () -> Listable) -> UIStackView {
-        if let list = build().asList() as? [UIView] {
-            list.forEach { view in
-                view.translatesAutoresizingMaskIntoConstraints = false
-                self.addArrangedSubview(view)
-            }
-        }
-        return self
-    }
-}
-
-
-public protocol HasApply { }
-
-extension HasApply {
-    public func apply(closure:(Self) -> ()) -> Self {
-        closure(self)
-        return self
-    }
-}
-
-extension UIView: HasApply { }
-
-//class HStack: UIStackView {
-//    init(@ListBuilder build: () -> Listable) {
-//        super.init(frame: .zero)
-//        self.axis = .horizontal
-//
-//        if let list = build().asList() as? [UIView] {
-//            list.forEach { view in
-//                view.translatesAutoresizingMaskIntoConstraints = false
-//                self.addArrangedSubview(view)
-//            }
-//        }
-//    }
-//
-//    required init(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//}
-//
-//class VStack: UIStackView {
-//    init(@ListBuilder build: () -> Listable) {
-//        super.init(frame: .zero)
-//        self.axis = .vertical
-//
-//        if let list = build().asList() as? [UIView] {
-//            list.forEach { view in
-//                view.translatesAutoresizingMaskIntoConstraints = false
-//                self.addArrangedSubview(view)
-//            }
-//        }
-//    }
-//
-//    required init(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//}
-
+#endif
